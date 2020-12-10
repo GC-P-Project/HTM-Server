e6fbb6dfc4155fa67d2381109b84885f736c381f
jjh5887 <wwjdrnjsgh159@naver.com>
2020-12-10 21:07:51 +0900

    HTMSVR-15 ExerciseService, ExerciseKindService 추가

---
+++ htm-api/src/main/java/com/gachon/htm/api/service/ExerciseService.java
@@ -0,0 +1,38 @@
+package com.gachon.htm.api.service;
+
+import com.gachon.htm.api.model.response.ExerciseListResponse;
+import com.gachon.htm.api.model.response.ExerciseVideoResponse;
+import com.gachon.htm.domain.model.ExerciseKind;
+import com.gachon.htm.domain.repository.Exercise.ExerciseRepository;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
+import org.springframework.stereotype.Service;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ExerciseService {
+
+    private final ExerciseRepository exerciseRepository;
+    private final ModelMapper modelMapper;
+
+    public ExerciseService(ExerciseRepository exerciseRepository, ModelMapper modelMapper) {
+        this.exerciseRepository = exerciseRepository;
+        this.modelMapper = modelMapper;
+    }
+
+    public List<ExerciseListResponse> find(List<ExerciseKind> list) {
+        List<ExerciseListResponse> responses = new ArrayList<>();
+        Type type = new TypeToken<List<ExerciseListResponse>>(){}.getType();
+        list.forEach(l -> {
+            responses.addAll(modelMapper.map(exerciseRepository.findByKind(l), type));
+        });
+        return responses;
+    }
+
+    public ExerciseVideoResponse find(long id) {
+        return modelMapper.map(exerciseRepository.findById(id).orElse(null), ExerciseVideoResponse.class);
+    }
+}
