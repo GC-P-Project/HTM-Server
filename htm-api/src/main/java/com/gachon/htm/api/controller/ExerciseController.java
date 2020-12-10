10a368abf0a56cedb6b01adbd7d36c915ec378d7
jjh5887 <wwjdrnjsgh159@naver.com>
2020-12-10 11:50:09 +0900

    HTMSVR-14 Exercise Response 추가

--- htm-api/src/main/java/com/gachon/htm/api/controller/ExerciseController.java
+++ htm-api/src/main/java/com/gachon/htm/api/controller/ExerciseController.java
@@ -1,19 +1,24 @@
 package com.gachon.htm.api.controller;
 
+import com.gachon.htm.api.model.response.ExerciseListResponse;
+import com.gachon.htm.api.model.response.ExerciseVideoResponse;
 import com.gachon.htm.domain.model.Exercise;
 import com.gachon.htm.domain.model.ExerciseKind;
-import com.gachon.htm.domain.repository.test.ExerciseKindRepository;
-import com.gachon.htm.domain.repository.test.ExerciseRepository;
+import com.gachon.htm.domain.repository.Exercise.ExerciseKindRepository;
+import com.gachon.htm.domain.repository.Exercise.ExerciseRepository;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/HTM")
@@ -21,14 +26,16 @@ public class ExerciseController {
 
     private final ExerciseRepository exerciseRepository;
     private final ExerciseKindRepository exerciseKindRepository;
+    private final ModelMapper modelMapper;
 
-    public ExerciseController(ExerciseRepository exerciseRepository, ExerciseKindRepository exerciseKindRepository) {
+    public ExerciseController(ExerciseRepository exerciseRepository, ExerciseKindRepository exerciseKindRepository, ModelMapper modelMapper) {
         this.exerciseRepository = exerciseRepository;
         this.exerciseKindRepository = exerciseKindRepository;
+        this.modelMapper = modelMapper;
     }
 
     @RequestMapping(value = "/list/{kind}", method = RequestMethod.GET)
-    public ResponseEntity<List<Exercise>> list(@PathVariable String kind) {
+    public ResponseEntity list(@PathVariable String kind) {
         List<ExerciseKind> byKindContains = exerciseKindRepository.findByKindContains(kind);
         if(byKindContains.isEmpty()) {
             return ResponseEntity.badRequest().build();
@@ -37,16 +44,19 @@ public class ExerciseController {
         byKindContains.forEach(k -> {
             list.addAll(exerciseRepository.findByKind(k));
         });
-        return ResponseEntity.ok(list);
+        Type type = new TypeToken<List<ExerciseListResponse>>(){}.getType();
+        List<ExerciseListResponse> responses = modelMapper.map(list, type);
+        return ResponseEntity.ok(responses);
     }
 
     @RequestMapping(value = "/video/{id}", method = RequestMethod.GET)
-    public ResponseEntity<Exercise> video(@PathVariable long id) {
+    public ResponseEntity video(@PathVariable long id) {
         Optional<Exercise> exercise = exerciseRepository.findById(id);
         if (!exercise.isPresent()) {
             return ResponseEntity.badRequest().build();
         }
-        return ResponseEntity.ok(exercise.get());
+        ExerciseVideoResponse response = modelMapper.map(exercise.get(), ExerciseVideoResponse.class);
+        return ResponseEntity.ok(response);
     }
 
 }
